8f2379ee51f6043a26220a64379bbc19aede0bb8
nichmidd <nick@solutionalchemy.io>
2018-11-06 11:16:15 +1100

    remove ssh test

--- createdevice.sh
+++ createdevice.sh
@@ -4,4 +4,3 @@ terraform init
 terraform plan -var "project_id=${project_id}" -var "auth_token=${PACKET_AUTH_TOKEN}" -out plan.tfstate
 terraform apply plan.tfstate
 export ip_address=$(terraform output maintenanceaddr)
-ssh root@$ip_address -oStrictHostKeyChecking=no -oUserKnownHostsFile=/dev/null 'hostname && uname -a'
