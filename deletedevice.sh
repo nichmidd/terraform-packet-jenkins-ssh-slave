cf4084f665a9476a56e2f3335dc9dd9b0056a8f9
nichmidd <nick@solutionalchemy.io>
2018-11-06 12:46:05 +1100

    and now we destroy

---
+++ deletedevice.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+#project_id=$(curl -X GET --header 'Accept: application/json' --header "X-Auth-Token: $PACKET_AUTH_TOKEN" 'https://api.packet.net/projects' | jq .projects[0].id)
+terraform destroy -var "project_id=${project_id}" -var "auth_token=${PACKET_AUTH_TOKEN}" -auto-approve
