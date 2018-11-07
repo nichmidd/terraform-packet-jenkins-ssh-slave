4935ce25261356a6df0547f6630f687cb54a723e
nichmidd <nick@solutionalchemy.io>
2018-11-08 07:07:27 +1100

    update project_id txt file name

--- deletedevice.sh
+++ deletedevice.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-project_id=`cat agent_id.txt`
+project_id=`cat project_id.txt`
 terraform destroy -var "project_id=${project_id}" -var "auth_token=${PACKET_AUTH_TOKEN}" -var "hostname=${BUILD_TAG}" -auto-approve
